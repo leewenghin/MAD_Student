7e782aa0753aab54f9f4488832bbe92258e2ee85
Lee Weng Hin <leewenghin1109@e.newera.edu.my>
2023-01-08 13:14:42 +0800

    update student

--- app/src/main/java/com/example/project/fragment/TitleFragment.kt
+++ app/src/main/java/com/example/project/fragment/TitleFragment.kt
@@ -1,6 +1,5 @@
 package com.example.project.fragment
 
-import android.content.Intent.getIntent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +8,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.project.MyAdapter
+import com.example.project.TitleAdapter
 import com.example.project.R
 import com.example.project.Submission
 import com.google.firebase.auth.FirebaseAuth
@@ -48,23 +47,127 @@ class TitleFragment: Fragment()  {
                 val userData = userSnapshot.data
                 val studentBatch = userData?.get("batch") as String
 
-                db.collection("submission")
+                val submissionReference = db.collection("submission")
                     .whereEqualTo("label", "Title").whereEqualTo("batch",studentBatch)
-                    .get()
-                    .addOnSuccessListener { submissionSnapshot ->
-                        if(!submissionSnapshot.isEmpty){
-                            for(data in submissionSnapshot.documents){
-                                val submission: Submission? = data.toObject(Submission::class.java)
-                                if(submission != null){
-                                    submissionList.add(submission)
-                                }
+
+                val submissionReference2 = db.collection("submission")
+
+                        submissionReference.get().addOnSuccessListener { submissionSnapshot ->
+
+                            // create a list to store the data of the subcollection
+                            val dataList = mutableListOf<Map<String, Any>>()
+                            dataList.clear()
+
+                             // store the data of the first document in a variable
+//                            val parentData = submissionSnapshot.first().data
+//                            Toast.makeText(context, dataList.joinToString(""), Toast.LENGTH_LONG).show()
+                            val i = 1
+
+                            for (document in submissionSnapshot) {
+                            // retrieve the data of the current document
+                            val currentDocumentId = document.id
+
+                                document.reference.collection("users").get()
+                                    .addOnSuccessListener { subcollectionSnapshot ->
+                                        if (!subcollectionSnapshot.isEmpty) {
+                                            for (data in subcollectionSnapshot) {
+                                                // store the loop data for subcollection in a variable
+                                                val parentData = document.data
+
+                                                // Toast.makeText(context, parentData.toString(), Toast.LENGTH_LONG).show()
+
+                                                // store the parentData data for subcollection in the list
+                                                dataList.add(parentData)
+
+                                                // Process the subdocument
+                                                val submission: Submission? =
+                                                    data.toObject(Submission::class.java)
+                                                if (submission != null) {
+                                                    submissionList.add(submission)
+                                                }
+                                            }
+                                            recyclerView.adapter = TitleAdapter(submissionList)
+                                        }
+                                        else {
+                                            val testing = i + 1
+
+                                            submissionReference.get()
+                                                .addOnSuccessListener { submissionSnapshot2 ->
+
+                                                    for (data in submissionSnapshot2) {
+                                                        Toast.makeText(context, testing.toString(), Toast.LENGTH_LONG).show()
+                                                        // retrieve the data of the current document
+                                                        val currentData = data.data
+
+                                                        // Toast.makeText(context, currentData.toString(), Toast.LENGTH_LONG).show()
+
+                                                        // check if the data of the current document is the same
+                                                        // as the stored data list
+                                                        if(dataList.contains(currentData)){
+                                                            // the submission is the same, skip the loop
+                                                            continue
+
+                                                        }else{
+                                                            // Process the subdocument
+                                                            val submission: Submission? =
+                                                                data.toObject(Submission::class.java)
+                                                            if (submission != null) {
+                                                                submissionList.add(submission)
+
+                                                            }
+                                                        }
+                                                    }
+                                                    recyclerView.adapter = TitleAdapter(submissionList)
+                                                }
+                                        }
+                                    }
                             }
-                            recyclerView.adapter = MyAdapter(submissionList)
                         }
-                    }
-                    .addOnFailureListener{
-                        Toast.makeText(context, it.toString(), Toast.LENGTH_LONG).show()
-                    }
+
+//                db.collection("submission")
+//                    .whereEqualTo("label", "Title").whereEqualTo("batch",studentBatch).get()
+//                    .addOnSuccessListener { testing ->
+//                        for (document in testing) {
+//                            document.reference.collection("users").get()
+//                                .addOnSuccessListener { subcollectionSnapshot ->
+//                                    if(!subcollectionSnapshot.isEmpty){
+//                                    for (data in subcollectionSnapshot) {
+//                                        // Process the subdocument
+//                                        val submission: Submission? =
+//                                            data.toObject(Submission::class.java)
+//                                        if (submission != null) {
+//                                            submissionList.add(submission)
+//                                        }
+//                                    }
+//                                    recyclerView.adapter = TitleAdapter(submissionList)
+//                                    }
+//                                }
+//                        }
+//                    }
+//                                .addOnFailureListener { exception ->
+//                                    // Handle the error
+//                                }
+//                        }
+//                    }
+
+//                db.collection("submission")
+//                    .whereEqualTo("label", "Title").whereEqualTo("batch",studentBatch)
+//                    .get()
+//                    .addOnSuccessListener { submissionSnapshot ->
+//                        if(!submissionSnapshot.isEmpty){
+//
+//                            for(data in submissionSnapshot.documents){
+//                                val submission: Submission? = data.toObject(Submission::class.java)
+//                                if(submission != null){
+//                                    submissionList.add(submission)
+//                                }
+//                            }
+//                            recyclerView.adapter = TitleAdapter(submissionList)
+//                        }
+//                    }
+//                    .addOnFailureListener{
+//                        Toast.makeText(context, it.toString(), Toast.LENGTH_LONG).show()
+//                    }
             }
         }
 
